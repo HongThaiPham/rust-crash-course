b8ab0d6016848de0572a370551e4325bc92f52a6
Pham Hong Thai <hongthaipro@gmail.com>
2022-04-29 13:50:32 +0700

    loops.rs

---
+++ src/loops.rs
@@ -0,0 +1,52 @@
+pub fn run() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    let mut count = 0;
+
+    loop {
+        count += 1;
+
+        println!("{}", count);
+
+        if count == 20 {
+            break;
+        }
+    }
+
+    // while loop
+    while count <= 100 {
+        if count % 15 == 0 {
+            println!("fizzbuzz");
+        } else if count % 3 == 0 {
+            println!("fizz");
+        } else if count % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", count);
+        }
+        count += 1;
+    }
+
+    // for range
+    for x in 0..100 {
+        if x % 15 == 0 {
+            println!("fizzbuzz");
+        } else if x % 3 == 0 {
+            println!("fizz");
+        } else if x % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
+}
